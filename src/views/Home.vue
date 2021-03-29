3571b2693fc64964a23e0c5ad5c16a5864e95bb6
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2021-03-29 14:01:15 +0900

    single project draft added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,30 @@
 <template>
-  <h1>Home</h1>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject.vue'
+
 export default {
   name: 'Home',
-  components: {
+  components: { SingleProject },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
   }
 }
 </script>
